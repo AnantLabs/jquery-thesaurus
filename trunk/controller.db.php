817a8f75acc1467c0b517924f13f74627929ec35
web.sbelarus <web.sbelarus@04d34808-d2d6-c6d9-81cc-27ee4d63566b>
2012-04-03 10:10:51 +0000

    Updated DB table dump file (text fields collation changed to utf8_general_c)

--- trunk/controller.db.php
+++ trunk/controller.db.php
@@ -9,21 +9,21 @@
 
 define('LIBPATH', dirname(__FILE__) . "/thesaurus/");
 try {
-include LIBPATH . "controller.php";
-include LIBPATH . "adapter/db.php";
+    include LIBPATH . "controller.php";
+    include LIBPATH . "adapter/db.php";
 
-$output = '';
-$th = new Thesaurus_Controller(new Thesaurus_Adapter_Db(
-   array(
-        "host" => "127.0.0.1",
-        "user" => "root",
-        "password" => "",
-        "dbname" => "thesaurus",
-    )
-), $output);
-$th->dispatch($_REQUEST);
+    $output = '';
+    $th = new Thesaurus_Controller(new Thesaurus_Adapter_Db(
+       array(
+            "host" => "127.0.0.1",
+            "user" => "root",
+            "password" => "",
+            "dbname" => "thesaurus",
+        )
+    ), $output);
+    $th->dispatch($_REQUEST);
 
-header("Cross-domain-access: true");
+    header("Cross-domain-access: true");
 ?>
 $.callbackData = {
     status : 'ok',
